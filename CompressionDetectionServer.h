d3d0869442c799cfb687dfd1c27ed310e0d9c11d
Ian Granger <icgranger9@gmail.com>
2019-03-05 04:49:57 -0800

    Uploading flies I worked on
    must still change location and add them to the wscript.

---
+++ CompressionDetectionServer.h
@@ -0,0 +1,75 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+
+ /*
+  Authors:
+    Ian Granger
+
+  Purpose:
+    Recieves two packet trains of 6000 packets each. 
+    From there, it looks at the arrival time of the first 
+    and last packet. If that arrival time > 100ms, then 
+    compression was detected.
+
+  Date:
+    2/25/19
+
+  Todo:
+    Everything
+  
+*/
+ 
+ #ifndef COMPRESSIONDETECTIONSERVER_H
+ #define COMPRESSIONDETECTIONSERVER_H
+ 
+ #include "ns3/udp-server.h"
+ #include "ns3/event-id.h"
+ #include "ns3/ptr.h"
+ #include "ns3/address.h"
+ #include "ns3/traced-callback.h"
+ #include "packet-loss-counter.h"
+ 
+ namespace ns3 {
+ class CompressionDetectionServer : public Application
+ {
+ public:
+   static TypeId GetTypeId (void);
+   CompressionDetectionServer ();
+   virtual ~CompressionDetectionServer ();
+
+   uint32_t GetLost (void) const;
+ 
+   uint64_t GetReceived (void) const;
+ 
+   uint16_t GetPacketWindowSize () const;
+ 
+   void SetPacketWindowSize (uint16_t size);
+ protected:
+   virtual void DoDispose (void);
+ 
+ private:
+ 
+   virtual void StartApplication (void);
+   virtual void StopApplication (void);
+ 
+   void HandleRead (Ptr<Socket> socket);
+   //void CheckCompression(uint64_t rxTimestamp); //function used to check if there has been compression
+ 
+   uint16_t m_port; 
+   Ptr<Socket> m_socket; 
+   Ptr<Socket> m_socket6; 
+   uint64_t m_received; 
+   PacketLossCounter m_lossCounter; 
+
+   //variables used to test compression
+   uint64_t m_rxHead;
+   uint64_t m_rxTail;
+ 
+   TracedCallback<Ptr<const Packet> > m_rxTrace;
+ 
+   TracedCallback<Ptr<const Packet>, const Address &, const Address &> m_rxTraceWithAddresses;
+ 
+ };
+ 
+ } // namespace ns3
+ 
+ #endif /* COMPRESSIONDETECTIONSERVER_H */
